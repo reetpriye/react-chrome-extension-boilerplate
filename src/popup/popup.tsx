db7fe6d3833174a621b98014d459042fd8b593bd
Reet Priye <reetpriye@gmail.com>
2021-11-17 23:37:46 +0530

    Add options & css-loader

---
+++ src/popup/popup.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './popup.css'
+
+const test = <img src="icon.png" />
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+ReactDOM.render(test, root)
--- src/test.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-const test = <p>Hello world!</p>
